4a9618e89dd17c709ee55c1530b30cea8fbcdc45
chaimaa.haji.etu@univ-lille.fr <chaimaahaji1998@gmail.com>
2023-01-10 18:49:32 +0100

    Last commit

---
+++ GildedRose.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using GildedRose;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {        
+        Shop shop = new Shop(new InMemoryItemsRepository());
+        shop.UpdateQuality();
+
+         
+    }
+}
